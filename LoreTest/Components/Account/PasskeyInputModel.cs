0b54aaa64382d1cbd429e989e031cbc806a59a2b
Linux <Linux@CONNIE>
2026-04-20 16:23:59 +0100

    Add project files.

---
+++ LoreTest/Components/Account/PasskeyInputModel.cs
@@ -0,0 +1,8 @@
+namespace LoreTest.Components.Account
+{
+    public class PasskeyInputModel
+    {
+        public string? CredentialJson { get; set; }
+        public string? Error { get; set; }
+    }
+}
---
+++ LoreTest/Components/Account/PasskeyOperation.cs
@@ -0,0 +1,8 @@
+namespace LoreTest.Components.Account
+{
+    public enum PasskeyOperation
+    {
+        Create = 0,
+        Request = 1,
+    }
+}
